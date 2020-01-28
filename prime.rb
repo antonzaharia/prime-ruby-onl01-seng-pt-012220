323cf5a3671c7182ee29467693adee5082f5d825
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-28 20:35:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
-def prime?(number)
-  n = 2
-  if number % n == 0 && number <= -1
-    
-    return false 
-  else
-    return true 
-  end
-  
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
 end
