93c8a58f9f53125f54ee36f05a7d6743334dce8d
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-28 20:33:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,10 @@
 def prime?(number)
-  (2..number-1).each {|n| return false if number <= -1 || number % n == 0}
-  return true
+  n = 2
+  if number % n == 0 && number <= -1
+    
+    return false 
+  else
+    return true 
+  end
+  
 end
