8d1574d33d2ee50e90aaa058de243bf8b95832ec
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-28 20:23:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,13 @@
 def prime?(number)
+
+if number == 0
   n = 2
-  if number % n == 0 && number <= 0 
+  if number % n == 0
     return false 
   else
     return true 
   end
-  
+  else
+  return false
+  end
 end
